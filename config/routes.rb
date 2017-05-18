ef29ccd2b862639c67b2e171f07637594246cf98
alinebc <abc30796@gmail.com>
2017-05-18 03:02:28 -0500

    Correcciones de links

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   
+  get 'mensajes/index'
+
   get 'arrendatario/index'
 
   get 'arrendador/index'
 
   get 'home/index'
+  get 'properties/index'
 
    get 'subir_casa/new'
    get 'subir_casa/index'
